cd91ae4b710af1e721560a33895965bd46d514b3
Saketh Vallakatla <saketh21v@gmail.com>
2017-10-02 02:03:30 +0530

    Updated the url to be relative to current location

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const wallpaper = require('wallpaper');
 // const url = "file:///E:/HPlace/Projects/NodeJs_Projects/DynamicWallpaper/Local/index.html";
 
 // const url = "file:///E:/HPlace/Projects/NodeJs_Projects/PhantomNode/file.html";
-const url = "file:///E:/HPlace/Projects/NodeJs_Projects/PhantomNode/Clock/index.html";
+const url = "Clock/index.html";
 
-var dun = async function() {
+var dun = async function () {
     const instance = await phantom.create();
     const page = await instance.createPage();
     // await page.property("viewportSize", {
@@ -16,14 +16,14 @@ var dun = async function() {
     //         height: 768
     //     })
     await page.property("viewportSize", {
-            width: 2560,
-            height: 1440
-        })
-        // await page.on('onResourceRequested', function(requestData) {
-        //     console.info('Requesting', requestData.url);
-        // });
+        width: 2560,
+        height: 1440
+    })
+    await page.on('onResourceRequested', function (requestData) {
+        console.info('Requesting', requestData.url);
+    });
     const status = await page.open(url);
-    var f = function() {
+    var f = function () {
         page.render('render.jpg');
         wallpaper.set('render.jpg');
         setTimeout(f, 1000)
