f2514b08322eb9952981f807356bd48220fb9b70
Saketh Vallakatla <saketh21v@gmail.com>
2017-10-02 01:46:52 +0530

    Overhaul. Using phantom package

---
+++ Clock/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/nDav/pen/zvPJem.
+
+ A simple clock made with JS and CSS
---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const phantom = require('phantom');
+const wallpaper = require('wallpaper');
+
+// const url = "file:///E:/HPlace/Projects/NodeJs_Projects/DynamicWallpaper/Local/index.html";
+
+// const url = "file:///E:/HPlace/Projects/NodeJs_Projects/PhantomNode/file.html";
+const url = "file:///E:/HPlace/Projects/NodeJs_Projects/PhantomNode/Clock/index.html";
+
+var dun = async function() {
+    const instance = await phantom.create();
+    const page = await instance.createPage();
+    // await page.property("viewportSize", {
+    //         width: 1366,
+    //         height: 768
+    //     })
+    await page.property("viewportSize", {
+            width: 2560,
+            height: 1440
+        })
+        // await page.on('onResourceRequested', function(requestData) {
+        //     console.info('Requesting', requestData.url);
+        // });
+    const status = await page.open(url);
+    var f = function() {
+        page.render('render.jpg');
+        wallpaper.set('render.jpg');
+        setTimeout(f, 1000)
+    };
+    f();
+    // await instance.exit();
+}
+
+dun();
