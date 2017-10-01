fae21e785c5c11333de1bb0ec61d210f55840195
Saketh Vallakatla <saketh21v@gmail.com>
2017-10-02 02:53:33 +0530

    Some code changes for manageability

--- index.js
+++ index.js
@@ -8,25 +8,34 @@ const wallpaper = require('wallpaper');
 // const url = "file:///E:/HPlace/Projects/NodeJs_Projects/PhantomNode/file.html";
 const url = "Clock/index.html";
 
+const _NORMAL = 1000;
+const _FAST = 500;
+const _VERY_FAST = 200;
+
+const _RENDER_FILE = "render.jpg";
+
 var dun = async function () {
     const instance = await phantom.create();
     const page = await instance.createPage();
     // await page.property("viewportSize", {
     //         width: 1366,
     //         height: 768
-    //     })
+    //     });
     await page.property("viewportSize", {
         width: 2560,
         height: 1440
-    })
-    await page.on('onResourceRequested', function (requestData) {
-        console.info('Requesting', requestData.url);
     });
+
+    await page.property('zoomFactor', 2);
+    // await page.on('onResourceRequested', function (requestData) {
+    //     console.info('Requesting', requestData.url);
+    // });
     const status = await page.open(url);
     var f = function () {
-        page.render('render.jpg');
-        wallpaper.set('render.jpg');
-        setTimeout(f, 1000)
+        page.render(_RENDER_FILE);
+        wallpaper.set(_RENDER_FILE);
+        // console.log("Rendered");
+        setTimeout(f, _FAST);
     };
     f();
     // await instance.exit();
